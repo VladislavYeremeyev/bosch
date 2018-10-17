22128cba85e18a93d49fa3c672252b135ca847cf
Kirill Erofeev <forlovesai@gmail.com>
2018-10-17 15:55:40 +0300

    transform

---
+++ transform.py
@@ -0,0 +1,20 @@
+def replaceStructure(newStruct, oldStruct, image):
+    """
+        Replace structure on the image
+
+        Parameters:
+        --------------------
+        newStruct: type?
+            Struct to replace oldStruct
+        oldStruct: type?
+            Struct to be replaced by newStruct
+        image: type?
+            Image with structure replaced by function
+
+        Return:
+        --------------------
+        structures : collection of structures
+            Set of structure from the images
+    """
+
+    return
